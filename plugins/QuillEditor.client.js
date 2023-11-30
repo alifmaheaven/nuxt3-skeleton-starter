3ce140c687bf55f7fd4aa6f66282312eb8bc7692
alifmaheaven <mohammad.maheaven@sigma.co.id>
2023-11-30 15:08:34 +0700

    update quilEditor

--- plugins/QuillEditor.client.js
+++ plugins/QuillEditor.client.js
@@ -1,53 +1,55 @@
-// under maintenance
+// QuillEditor
+// Mau nambah module? https://vueup.github.io/vue-quill/guide/modules.html
 
-// // QuillEditor
-// // Mau nambah module? https://vueup.github.io/vue-quill/guide/modules.html
-
-// import { QuillEditor, Quill } from "@vueup/vue-quill";
-// import BlotFormatter from "quill-blot-formatter";
-// import ImageUploader from "quill-image-uploader";
-// import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import { QuillEditor, Quill } from '@vueup/vue-quill'
+// import BlotFormatter from 'quill-blot-formatter'
+import BlotFormatter from 'quill-blot-formatter/dist/BlotFormatter';
+// import ImageUploader from 'quill-image-uploader';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  //   const modules = [
-  //     {
-  //       name: "blotFormatter",
-  //       module: BlotFormatter,
-  //       options: {},
-  //     },
-  //     // kalo pas upload image mau diupload dulu ke server
-  //     // {
-  //     //   name: 'imageUploader',
-  //     //   module: ImageUploader,
-  //     //   options: {
-  //     //     upload: file => {
-  //     //       return new Promise((resolve, reject) => {
-  //     //         const formData = new FormData();
-  //     //         formData.append("image", file);
-  //     //         useNuxtApp().$api.post('/upload-image', formData)
-  //     //           .then(res => {
-  //     //             console.log(res)
-  //     //             resolve(res.data.url);
-  //     //           })
-  //     //           .catch(err => {
-  //     //             reject("Upload failed");
-  //     //             console.error("Error:", err)
-  //     //           })
-  //     //       })
-  //     //     }
-  //     //   }
-  //     // }
-  //   ];
-  //   const globalOptions = {
-  //     debug: "info",
-  //     modules: {
-  //       table: true, // disable table module
-  //     },
-  //     theme: "snow",
-  //   };
-  //   // set default globalOptions prop
-  //   QuillEditor.props.globalOptions.default = () => globalOptions;
-  //   QuillEditor.props.modules.default = () => modules;
-  //   // register component
-  //   nuxtApp.vueApp.component("QuillEditor", QuillEditor);
-});
+  const modules = [
+    {
+      name: 'blotFormatter',
+      module: BlotFormatter,
+      options: {}
+    },
+    // kalo pas upload image mau diupload dulu ke server
+    // {
+    //   name: 'imageUploader',
+    //   module: ImageUploader,
+    //   options: {
+    //     upload: file => {
+    //       return new Promise((resolve, reject) => {
+    //         const formData = new FormData();
+    //         formData.append("image", file);
+
+    //         useNuxtApp().$api.post('/upload-image', formData)
+    //           .then(res => {
+    //             console.log(res)
+    //             resolve(res.data.url);
+    //           })
+    //           .catch(err => {
+    //             reject("Upload failed");
+    //             console.error("Error:", err)
+    //           })
+    //       })
+    //     }
+    //   }
+    // }
+  ]
+
+  const globalOptions = {
+    debug: 'info',
+    modules: {
+      table: true,  // disable table module
+    },
+    theme: 'snow'
+  }
+  // // set default globalOptions prop
+  QuillEditor.props.globalOptions.default = () => globalOptions;
+  QuillEditor.props.modules.default = () => modules;
+
+  // // register component
+  nuxtApp.vueApp.component('QuillEditor', QuillEditor)
+})
